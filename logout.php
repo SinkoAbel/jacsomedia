15cc1ad480ebb70cecfa9cb0e5b83f1f382f2cef
Ábel Sinkó <84632718+SinkoAbel@users.noreply.github.com>
2022-02-25 21:14:34 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@
     session_unset();
     session_destroy();
 
-    echo '<meta http-equiv="refresh" content="0, url=index.php">';
+    echo '<meta http-equiv="refresh" content="0, url=index.php?page=login">';
 
 
 ?>
